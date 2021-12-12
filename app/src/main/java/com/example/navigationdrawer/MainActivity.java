c446dea1e9cc36b690e40ca651db0936ff3b0254
UsmanJavaid008 <bcsf18m008@pucit.edu.pk>
2021-12-12 12:41:06 +0500

    Back Button Event

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -1,11 +1,33 @@
 package com.example.navigationdrawer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+@Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+            Toast.makeText(getApplicationContext(),"Start",Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"End",Toast.LENGTH_LONG).show();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
